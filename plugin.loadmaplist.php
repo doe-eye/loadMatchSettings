8814442ab8ff9c9bd665c6f2a75789b57fb2a463
doe-eye <doe-eye@gmx.net>
2014-06-25 12:21:49 +0200

    Update plugin.loadmaplist.php

--- plugin.loadmaplist.php
+++ plugin.loadmaplist.php
@@ -1,6 +1,6 @@
 <?php
 /*********************************************************************************
-Plugin lml.php
+Plugin lml.php v.3
 Lists the maplists in the MatchSettings folder in a manialink
 Loads the clicked maplist
 
@@ -9,28 +9,27 @@ some code taken and adapted from plugin.rasp_jukebox.php
 *********************************************************************************/
 
 
-Aseco::addChatCommand('listml', 'list maplists');
+Aseco::addChatCommand('/listml', 'list maplists');
 Aseco::registerEvent('onPlayerManialinkPageAnswer', 'event_lml');
 
 //triggered, when someone clicks in the displayed maplist-manialink
 function event_lml($aseco, $answer){
 	$login = $answer[1];
 	$action = $answer[2];
-	$aseco->console('eventaction: '.$action);
 	
 	if(substr($action, -4, 4) == ".txt"){//only if a matchsettings-file is clicked
 		$rtn = $aseco->client->query('LoadMatchSettings', "MatchSettings/$action");
 		if(!$rtn){
 			trigger_error('[' . $aseco->client->getErrorCode() . '] ' . $aseco->client->getErrorMessage(), E_USER_WARNING);
 			$aseco->console($aseco->client->getErrorMessage());
-			$aseco->client->query('ChatSendServerMessage', $aseco->client->getErrorMessage());
+			$aseco->client->query('ChatSendServerMessageToLogin', $aseco->client->getErrorMessage(), $login);
 		}
 		else{
-			$aseco->console("MatchSettings/$action loaded by $login");
-			$aseco->client->query('ChatSendServerMessage', "MatchSettings MatchSettings/$action loaded by $login");
+			$aseco->console("$action loaded by $login");
+			$aseco->client->query('ChatSendServerMessage', "MatchSettings $action loaded by $login");
 		}
 	}
-	elseif(substr($action, -1, 1) == '/' ){//if a directory is clicked
+	elseif(substr($action, -1, 1) == '/'){//if a directory is clicked
 		$player = $aseco->server->players->getPlayer($login);
 		$command = array();
 		$command['author'] = $player;
@@ -45,18 +44,20 @@ function lml_fetchMatchsettings($aseco, $dir){
 	$msArray = array();
 	$mapsDir = $aseco->server->mapdir;
 	
-	//take files from folder "MatchSettings"
-	$msDir = $mapsDir.='MatchSettings';
-	//or if specified a subfolder
-	$msDir .= "/$dir";
+	//specified a subfolder
+	$msDir = $mapsDir.'MatchSettings/'.$dir;
 	
 	$msDirHandle = opendir($msDir);
 	while($file = readdir($msDirHandle)){
-		if($file != '.' && $file != '..'){
+		if($file != '.'){
 			if(substr($file, -4, 4) != ".txt"){//is it a directory
 				$file = $file."/";
 			}
-			array_push($msArray, $file);
+			//only show directory up, when not on toplevel
+			if(!($dir == '' && $file == '../')){
+				array_push($msArray, $file);
+			}
+			
 		}
 	}
 	closedir($msDirHandle);
@@ -68,54 +69,64 @@ function lml_fetchMatchsettings($aseco, $dir){
 
 
 
-//called, when "/listml" ist typed in chat
+//called, when "/listml" ist typed in chat by a (Master)Admin
 function chat_listml($aseco, $command) {
 
 	$player = $command['author'];
-	$login = $player->login;
-	$subfolder = '';
-	$arglist = explode(' ',  $command['params']);
 	
+	if($aseco->isMasterAdmin($player) || $aseco->isAdmin($player)){
 	
-	if($arglist[0]!= ''){
-		$subfolder = $arglist[0];
-		$aseco->console('param: ' .$subfolder);
-	}
-	$msFileArray = lml_fetchMatchsettings($aseco, $subfolder);
-
-
-	
-	$head = "Maplists in UserData/MatchSettings$subfolder:";
-	$page = array();
-	if ($aseco->settings['clickable_lists'])
-		$page[] = array('$i$oId', '$i$oName $f00(click to load)');
-	else
-		$page[] = array('Id', 'Name');
-
-	$tid = 1;
-	$lines = 0;
-	$player->msgs = array();
-	$player->msgs[0] = array(1, $head, array(1.10, 0.1, 0.6), array('Icons128x128_1', 'Load', 0.02));
-	
-	foreach ($msFileArray as $item) {
-		$page[] = array(str_pad($tid, 2, '0', STR_PAD_LEFT), array($item, "$subfolder"."$item"));//action
-		$tid++;
+		$folder = '';
+		$arglist = explode(' ',  $command['params']);
 		
-		if (++$lines > 14) {
-			$player->msgs[] = $page;
-			$lines = 0;
-			$page = array();
-				if ($aseco->settings['clickable_lists'])
-					$page[] = array('$i$oId', '$i$oName $f00(click to load)');
-				else
-					$page[] = array('Id', 'Name');
+		
+		
+		if($arglist[0]!= ''){
+			$folder = $arglist[0];
+			if(substr($folder, -3, 3) == '../'){//when directory-up
+				$pathArray = explode('/', $folder);
+				$folder = '';
+				$count = count($pathArray);
+				for($i = 0; $i < $count-3; $i++){
+					$folder .= $pathArray[$i].'/';
+				}
+			}
 		}
+		
+		$msFileArray = lml_fetchMatchsettings($aseco, $folder);
+		
+		$head = "Maplists in MatchSettings/$folder";
+		$page = array();
+		if ($aseco->settings['clickable_lists'])
+			$page[] = array('$i$oId', '$i$oName $f00(click to load)');
+		else
+			$page[] = array('Id', 'Name');
+
+		$tid = 1;
+		$lines = 0;
+		$player->msgs = array();
+		$player->msgs[0] = array(1, $head, array(1.10, 0.1, 0.6), array('Icons128x128_1', 'Load', 0.02));
+		
+		foreach ($msFileArray as $item) {
+			$page[] = array(str_pad($tid, 2, '0', STR_PAD_LEFT), array($item, $folder.$item));//action
+			$tid++;
+			
+			if (++$lines > 14) {
+				$player->msgs[] = $page;
+				$lines = 0;
+				$page = array();
+					if ($aseco->settings['clickable_lists'])
+						$page[] = array('$i$oId', '$i$oName $f00(click to load)');
+					else
+						$page[] = array('Id', 'Name');
+			}
+		}
+		if (count($page) > 1) {
+			$player->msgs[] = $page;
+		}
+		// display
+		display_manialink_multi($player);
 	}
-	if (count($page) > 1) {
-		$player->msgs[] = $page;
-	}
-	// display
-	display_manialink_multi($player);
 } 
 
 
